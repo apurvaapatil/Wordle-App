c1ac636ec685754170345956c08b373277a4d26e
apurvaapatil <apoorvapatil03@gmail.com>
2022-07-24 16:58:01 +0530

    changes to display error message for invalid word

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import useWordle from '../hooks/useWordle';
 
 import './keypad.css';
 
--- src/context/context.js
+++ src/context/context.js
@@ -2,6 +2,6 @@ import React, { createContext } from 'react'
 
 const ModalContext = createContext();
 
-// export const RowContext = createContext();
+export const RowContext = createContext();
 
 export default ModalContext;
